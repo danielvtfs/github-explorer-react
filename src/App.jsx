e5187d21ecfce5ef0c522ef539b69576148ce7d6
danielvtfs <danielvtfs@gmail.com>
2022-03-25 10:45:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { RepositoryList } from "./components/repositoryList";
+
+import "./styles/global.scss";
+
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
+}
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositiryItem(props) {
+  return (
+    <li>
+      <strong>{props.repository?.name ?? "Default"}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.link}>Acessar Repo</a>
+    </li>
+  );
+}
